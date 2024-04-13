4b08c787132a0593a3d9a5b084e81aed68364e04
lastemp <emmanuelochieng797@gmail.com>
2024-04-13 17:24:35 +0300

    4th commit

--- programs/solar_crowd_funding/src/state/investor.rs
+++ programs/solar_crowd_funding/src/state/investor.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 #[account]
 #[derive(Default, Debug, InitSpace)]
 pub struct Investor {
-    pub owner: Pubkey, // publickey of the tree owner
+    pub owner: Pubkey, // publickey of the investor
     #[max_len(50)]
     pub full_names: String, // full names i.e first name, middlename, surname
     #[max_len(3)]
